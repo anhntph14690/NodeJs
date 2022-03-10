a22f60778edac91b5c3cf126f5a501eceb03d287
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-03-10 15:21:04 +0700

    Buổi 2: NoteJS

--- src/app.js
+++ src/app.js
@@ -2,52 +2,25 @@
 
 const express = require('express');
 const cors = require('cors');
+import productsRoute from './routes/product';
+import morgan from 'morgan';
 
 const app = express();
+//middleware
 app.use(cors())
+app.use(morgan('tiny'))
+app.use(express.json())
 
-const checkAuth = (req, res, next) => { 
-    const isAdmin = true;
-    if(isAdmin){
-        next();
-    }else {
-        console.log('Chim cút');
-    }
-}
 
-app.get('/', (req, res) => {
-    res.send("<h1>Home Page</h1>");
-})
-app.get('/api/products', checkAuth, (req, res) => {
-    const data = [
-            {id: 1, name: "Product A"},
-            {id: 2, name: "Product B" },
-        ];
-        res.json(data);
-})
 
-// const server = http.createServer((req, res) => {
-//     const url = req.url;
-//     console.log(url);
-//     if(url === "/api/products"){
-//         const data = [
-//             {id: 1, name: "Product A"},
-//             {id: 2, name: "Product B" },
-//         ];
-//         res.end(JSON.stringify(data))
-//     }else if(url === "/api/post"){
-//         console.log("API Post")
-//     }else{
-//         res.setHeader("Content-Type", "text/html");
-//         res.write("<html><body><h1>Home Page</h1></body></html>");
-//         res.end();
 
-//     }
-//     // console.log("Tạo server thành công!");
-// })
+//routes
+app.use("/api", productsRoute)
 
+
+//content
 const PORT = 3001;
-server.listen(PORT, ()=> {
+app.listen(PORT, ()=> {
     console.log("Server của bạn đang chạy cổng ", PORT);
 });
 
