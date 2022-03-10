9e074aeebd9a1dd0fc33ed9ecc9f87ac22ae986d
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-03-10 15:39:51 +0700

    Buổi 2: Update

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use("/api", productsRoute)
 
 
-//content
+//connect
 const PORT = 3001;
 app.listen(PORT, ()=> {
     console.log("Server của bạn đang chạy cổng ", PORT);
