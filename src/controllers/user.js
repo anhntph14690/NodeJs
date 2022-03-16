bfacda4b0887af844a27ecd18d50f69f2be2c9de
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-03-17 00:41:00 +0700

    Update User: Signin

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,6 +4,17 @@ export const signup = async (req, res) => {
         const user = await new User(req.body).save();
         res.json(user);
     } catch (error) {
-        res.status(400).json({ message: "Lỗi" });
+        res.status(400).json({ message: "User chưa được tao!" });
     }
 };
+export const signin = async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.params.id }).exec();
+        res.json(user);
+    } catch (error) {
+        res.status(400).json({
+            error: "Đăng nhập không thành công!"
+        })
+    }
+}
+
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { signup } from '../controllers/user';
-
+import { signup, signin } from '../controllers/user';
+import { checkAuth } from '../middlewares/checkAuth';
 
 const router = Router();
 
 router.post("/signup", signup);
+router.get("/signin/:id", signin);
+
 
 export default router;
